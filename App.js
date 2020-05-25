b583ba730f1b347527f11c4b9d4014b250add234
solidw <solidw@naver.com>
2020-05-25 23:39:18 +0900

    refactor: Change dir structure for Navigator

--- App.js
+++ App.js
@@ -8,46 +8,14 @@ import {
   StatusBar,
   Image,
 } from 'react-native';
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-
-import HomeScreen from 'src/screen/HomeScreen';
-import MyInfoScreen from 'src/screen/MyInfoScreen';
-import CalendarScreen from 'src/screen/CalendarScreen';
 import styled from 'styled-components';
 
-const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
+import MainTabNavigator from 'src/screens/MainTabNavigator';
 
 const App = () => {
   return (
     <SafeAreaViewWrapper>
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = focused
-                  ? 'android-information-circle'
-                  : 'android-information-circle-outline';
-              } else if (route.name === 'MyInfo') {
-                iconName = focused ? 'android-list-box' : 'android-list';
-              }
-              return <Image name={iconName} size={size} color={color} />; // ionIcon
-            },
-          })}
-          tabBarOptions={{
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-          }}>
-          <Tab.Screen name="Home" component={HomeScreen} />
-          <Tab.Screen name="MyInfo" component={MyInfoScreen} />
-          <Tab.Screen name="Calendar" component={CalendarScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <MainTabNavigator />
     </SafeAreaViewWrapper>
   );
 };
