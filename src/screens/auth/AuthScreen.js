2642b2e12062cb7c6db0d5a6481e139a4f802c29
solidw <solidw@naver.com>
2020-05-26 04:15:54 +0900

    feat: Prepare for auth stack navigator

---
+++ src/screens/auth/AuthScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styled from 'styled-components';
+import palette from 'src/lib/palette';
+
+const AuthScreen = () => {
+  return (
+    <AuthScreenWrapper>
+      <Text>AuthScreen</Text>
+    </AuthScreenWrapper>
+  );
+};
+const AuthScreenWrapper = styled.View``;
+
+export default AuthScreen;
