5a0951804e2b385a6b5f97893a3ced4fd6391520
solidw <solidw@naver.com>
2020-06-01 03:08:36 +0900

    refactor: Use CustomWebView component in SNSScreen

--- src/screens/tab/SNSScreen.js
+++ src/screens/tab/SNSScreen.js
@@ -1,34 +1,8 @@
-import React, { useState } from 'react';
-import { ActivityIndicator, View } from 'react-native';
-import { WebView } from 'react-native-webview';
-import styled from 'styled-components';
+import React from 'react';
+import CustomWebView from 'src/components/CustomWebView';
 
 const SNSScreen = () => {
-  const [visible, setVisible] = useState(true);
-  return (
-    <SNSView>
-      <WebView
-        source={{ uri: 'https://www.instagram.com/gogo_hakgyo/?hl=ko' }}
-        onLoadStart={() => setVisible(true)}
-        onLoad={() => setVisible(false)}
-      />
-      {visible && <SNSActivityIndicator size="large" />}
-    </SNSView>
-  );
+  return <CustomWebView uri={'https://www.instagram.com/gogo_hakgyo/?hl=ko'} />;
 };
 
-const SNSView = styled.View`
-  flex: 1;
-`;
-
-const SNSActivityIndicator = styled.ActivityIndicator`
-  flex: 1;
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  position: absolute;
-  align-items: center;
-  justify-content: center;
-`;
 export default SNSScreen;
