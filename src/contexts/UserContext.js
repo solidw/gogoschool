befcabb6e6b4f63dfb989351520911a0a1897081
solidw <solidw@naver.com>
2020-06-01 03:09:27 +0900

    feat: Add example User student or teacher

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,20 +1,43 @@
+// 임시 db
 import React, { useState, createContext } from 'react';
 
 export const UserContext = createContext();
 
 const UserContextProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    name: '고태완',
-    isStudent: true,
-  });
+  const userExample = {
+    studentExample: {
+      name: '고태완',
+      isStudent: true,
+      region: '대전',
+      code: 'AAABBB',
+    },
+    teacherExample: {
+      name: '신민철',
+      isStudent: false,
+      region: '대구',
+      code: 'CCCDDD',
+    },
+  };
+
+  const [user, setUser] = useState(userExample.studentExample);
 
   const toggleUser = () => {
-    setUser({ ...user, isStudent: !user.isStudent });
-    console.log(user.isStudent);
+    setUser(
+      user.isStudent ? userExample.teacherExample : userExample.studentExample,
+    );
+    console.log(user);
+  };
+
+  const loginWith = something => {
+    if (something === 'student') {
+      setUser(userExample.studentExample);
+    } else if (something === 'teacher') {
+      setUser(userExample.teacherExample);
+    }
   };
 
   return (
-    <UserContext.Provider value={{ ...user, toggleUser }}>
+    <UserContext.Provider value={{ user, toggleUser, loginWith }}>
       {children}
     </UserContext.Provider>
   );
