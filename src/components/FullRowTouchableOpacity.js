3252640330ebfb7cb4064810a979e2019eb052ce
solidw <solidw@naver.com>
2020-06-01 03:10:55 +0900

    feat: FullRow Button use in authentication screen

---
+++ src/components/FullRowTouchableOpacity.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
+import palette from 'src/lib/palette';
+
+const FullRowTouchableOpacity = ({ text, background, onPress }) => {
+  return (
+    <FullRowTouchableOpacityWrapper background={background} onPress={onPress}>
+      <ColoredText>{text}</ColoredText>
+    </FullRowTouchableOpacityWrapper>
+  );
+};
+
+const FullRowTouchableOpacityWrapper = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  padding: 15px 0px;
+  margin: 10px;
+  border-radius: 8px;
+
+  background-color: ${props =>
+    props.background ? props.background : palette.hakgyoPurple};
+`;
+
+const ColoredText = styled.Text`
+  color: ${props => (props.color ? props.color : palette.white)};
+`;
+
+export default FullRowTouchableOpacity;
