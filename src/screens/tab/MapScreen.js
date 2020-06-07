f233a15132b7049120b4d0f49b1f1762237da6a7
solidw <solidw@naver.com>
2020-06-07 17:11:44 +0900

    feat: Add react-native-maps but there is problem at version 0.27.1, so the version is git+github.com/react-native-community/react-native-maps

--- src/screens/tab/MapScreen.js
+++ src/screens/tab/MapScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 const MapScreen = () => {
   return (
     <MapScreenWrapper>
-      <Text>MapScreen</Text>
+      <MapView style={{ flex: 1 }} provider={PROVIDER_GOOGLE} />
     </MapScreenWrapper>
   );
 };
-const MapScreenWrapper = styled.View``;
+const MapScreenWrapper = styled.View`
+  flex: 1;
+`;
 
 export default MapScreen;
