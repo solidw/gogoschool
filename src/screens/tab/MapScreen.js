73407b2cec2451275176f5f8a68db4b1c428a996
solidw <solidw@naver.com>
2020-06-09 01:55:28 +0900

    feat: Add react-native-maps, geolocation-service for google maps

--- src/screens/tab/MapScreen.js
+++ src/screens/tab/MapScreen.js
@@ -1,12 +1,54 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, PermissionsAndroid } from 'react-native';
 import styled from 'styled-components';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
 
 const MapScreen = () => {
+  const [location, setLocation] = useState(undefined);
+
+  useEffect(() => {
+    const requestPermissions = async () => {
+      await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+    };
+    requestPermissions();
+    Geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        setLocation({
+          latitude,
+          longitude,
+        });
+      },
+      error => {
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 },
+    );
+  }, []);
+
   return (
     <MapScreenWrapper>
-      <MapView style={{ flex: 1 }} provider={PROVIDER_GOOGLE} />
+      {location && (
+        <MapView
+          style={{ flex: 1 }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        </MapView>
+      )}
     </MapScreenWrapper>
   );
 };
