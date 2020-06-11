c788a090fe9cf6b13ded3a3c46dd8466a9333811
solidw <solidw@naver.com>
2020-06-12 00:29:01 +0900

    chore: Remove unused import statement

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,16 +1,13 @@
 import 'react-native-gesture-handler';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
 import styled from 'styled-components';
 import AsyncStorage from '@react-native-community/async-storage';
 
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
 
 import MainTabNavigator from 'src/navigators/MainTabNavigator';
-import { UserContext } from 'src/contexts/UserContext';
 import { AuthContext } from 'src/contexts/AuthContext';
-import AuthScreen from 'src/screens/auth/AuthScreen';
 import AuthStackNavigator from './navigators/AuthStackNavigator';
 
 const Stack = createStackNavigator();
