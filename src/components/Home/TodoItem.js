9b96b2789539efa1943fec1cc7215346070cc9d4
solidw <solidw@naver.com>
2020-06-13 05:21:52 +0900

    feat: Add Icon and StyledText for TodoMission

--- src/components/Home/TodoItem.js
+++ src/components/Home/TodoItem.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
 import palette from 'src/lib/palette';
-import TodoIcon from 'src/components/Home/TodoIcon';
-
+import Icon from 'src/components/Home/Icon';
+import StyledText from 'src/components/StyledText';
+import CheckedBox from 'src/lib/assets/checked_box.png';
+import UnCheckedBox from 'src/lib/assets/unchecked_box.png';
 const TodoItem = ({ item, addTenProgress, iconSource }) => {
   const [isDone, setDone] = useState(false);
   const onPressItem = () => {
@@ -14,21 +16,25 @@ const TodoItem = ({ item, addTenProgress, iconSource }) => {
   };
   return (
     <TodoItemWrapper isDone={isDone} onPress={onPressItem} disabled={isDone}>
-      <Text>{item}</Text>
-      <TodoIcon source={iconSource} />
-      <Text>{isDone ? 'O' : 'X'}</Text>
+      <StyledText size={20}>{item}</StyledText>
+      <Icon size={30} source={iconSource} />
+      <Icon size={20} source={isDone ? CheckedBox : UnCheckedBox} />
     </TodoItemWrapper>
   );
 };
 const TodoItemWrapper = styled.TouchableOpacity`
-  flex-basis: 50%;
+  height: 30%;
+  flex-basis: 45%;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  align-content: space-around;
   border: 1px solid black;
+  margin: 3px;
   border-radius: 5px;
-  padding-horizontal: 10px;
+  padding-horizontal: 7px;
   padding-vertical: 5px;
-  background-color: ${props => (props.isDone ? palette.hakgyoPurple : 'white')};
+  background-color: ${props => (props.isDone ? palette.hakgyoYellow : 'white')};
 `;
 
 styled.Text``;
