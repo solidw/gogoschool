2e6c72140751ca05ea92addd4c1a87e8323ba846
solidw <solidw@naver.com>
2020-06-12 20:43:51 +0900

    feat: Automate after load for student name, code

--- src/screens/stack/SelfCheckScreen.js
+++ src/screens/stack/SelfCheckScreen.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import CustomWebView from 'src/components/CustomWebView';
 
 const SelfCheckScreen = () => {
-  return <CustomWebView uri={'https://eduro.dge.go.kr/hcheck/index.jsp'} />;
+  const isAndroid = Platform.OS === 'android';
+  const studentJsCode = `
+    document.getElementById('pName').value="강경준";
+    document.getElementById('qstnCrtfcNo').value="THNR28";
+    document.getElementById('btnConfirm').click();
+  `;
+  const teacherJsCode = `
+      document.getElementById('pName').value="신민철";
+      document.getElementById('qstnCrtfcNo').value="ANGX8U";
+      document.getElementById('btnConfirm').click();
+
+      document.getElementById('srchGrade').value = 4;
+      const classCode = document.getElementById('srchClassCode');
+      classCode.value = classCode[1].value;
+      document.getElementById('btnDtlSearch').click();
+    `;
+  return (
+    <CustomWebView
+      uri={'https://eduro.dge.go.kr/stv_cvd_co00_010.do'}
+      javaScriptEnabledAndroid={true}
+      injectedJavaScript={studentJsCode}
+      scalesPageToFit={false}
+      scrollEnabled
+    />
+  );
 };
 
 export default SelfCheckScreen;
