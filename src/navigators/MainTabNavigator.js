8a9360c90a240ec3b70a6d68c8c06c4b89dd4043
solidw <solidw@naver.com>
2020-06-13 05:32:02 +0900

    feat: Apply last-shape

--- src/navigators/MainTabNavigator.js
+++ src/navigators/MainTabNavigator.js
@@ -7,17 +7,16 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapScreen from 'src/screens/tab/MapScreen';
 import CalendarScreen from 'src/screens/tab/CalendarScreen';
 import SNSScreen from 'src/screens/tab/SNSScreen';
-import NoticeScreen from 'src/screens/tab/NoticeScreen';
-import ChatbotScreen from 'src/screens/tab/ChatbotScreen';
 import HomeStackNavigator from 'src/navigators/HomeStackNavigator';
+import MyPageScreen from 'src/screens/tab/MyPageScreen';
+
 import Icon from 'src/components/Home/Icon';
-import TabIcon from 'src/components/Home/TabIcon';
 
 import GPS from 'src/lib/assets/gps.png';
 import Calendar from 'src/lib/assets/calendar.png';
 import Home from 'src/lib/assets/home.png';
-import Chatbot from 'src/lib/assets/chatbot.png';
 import Notice from 'src/lib/assets/notice.png';
+import MyPage from 'src/lib/assets/user.png';
 
 const Tab = createBottomTabNavigator();
 
@@ -36,30 +35,23 @@ const MainTabNavigator = () => {
               iconName = Calendar;
             } else if (route.name === '알림장') {
               iconName = Notice;
-            } else if (route.name === '챗봇') {
-              iconName = Chatbot;
+            } else if (route.name === '마이페이지') {
+              iconName = MyPage;
             }
-            return <TabIcon source={iconName} />; // ionIcon
+            return <Icon size={40} source={iconName} />; // ionIcon
           },
         })}
         tabBarOptions={{
           activeTintColor: 'tomato',
           inactiveTintColor: 'gray',
+          style: { height: '10%' },
         }}
         initialRouteName={'홈'}>
         <Tab.Screen name="동선" component={MapScreen} />
-        <Tab.Screen
-          name="등교일"
-          component={CalendarScreen}
-          initialParams={{ isStudent: true }}
-        />
-        <Tab.Screen
-          name="홈"
-          component={HomeStackNavigator}
-          initialParams={{ isStudent: true }}
-        />
+        <Tab.Screen name="등교일" component={CalendarScreen} />
+        <Tab.Screen name="홈" component={HomeStackNavigator} />
         <Tab.Screen name="알림장" component={SNSScreen} />
-        <Tab.Screen name="챗봇" component={ChatbotScreen} />
+        <Tab.Screen name="마이페이지" component={MyPageScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
