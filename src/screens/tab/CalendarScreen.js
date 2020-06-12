f4c6bc836316d9c321dbbe3e96258e84f429fbbe
solidw <solidw@naver.com>
2020-06-13 05:30:28 +0900

    feat: Apply stretch View and add dummy data

--- src/screens/tab/CalendarScreen.js
+++ src/screens/tab/CalendarScreen.js
@@ -8,10 +8,24 @@ import {
   LocaleConfig,
 } from 'react-native-calendars';
 import { getFormatDate } from 'src/lib/Date.js';
+import palette from 'src/lib/palette';
 
 const CalendarScreen = () => {
   const now = new Date();
-  const dates = [getFormatDate(now), '2020-05-29', '2020-06-01'];
+  const dates = [
+    getFormatDate(now),
+    '2020-06-01',
+    '2020-06-03',
+    '2020-06-05',
+    '2020-06-09',
+    '2020-06-11',
+    '2020-06-15',
+    '2020-06-17',
+    '2020-06-19',
+    '2020-06-23',
+    '2020-06-25',
+    '2020-06-29',
+  ];
   const objForProps = {};
   dates.forEach(
     item =>
@@ -22,7 +36,15 @@ const CalendarScreen = () => {
         textColor: 'white',
       }),
   );
-  return <Calendar markedDates={objForProps} markingType={'period'} />;
+  return (
+    <CalendarScreenWrapper>
+      <Calendar markedDates={objForProps} markingType={'period'} />
+    </CalendarScreenWrapper>
+  );
 };
 
+const CalendarScreenWrapper = styled.View`
+  flex: 1;
+  background-color: ${palette.white};
+`;
 export default CalendarScreen;
