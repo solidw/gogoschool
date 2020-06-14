32388886847941c3d2d03a05b20de3d4928abf19
solidw <solidw@naver.com>
2020-06-14 23:44:13 +0900

    feat: Make this react component to styled component

--- src/components/FullRowTouchableOpacity.js
+++ src/components/FullRowTouchableOpacity.js
@@ -1,32 +1,16 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
 import palette from 'src/lib/palette';
-import StyledText from 'src/components/StyledText';
 
-const FullRowTouchableOpacity = ({ text, background, onPress }) => {
-  return (
-    <FullRowTouchableOpacityWrapper background={background} onPress={onPress}>
-      <StyledText size={20}>{text}</StyledText>
-    </FullRowTouchableOpacityWrapper>
-  );
-};
-
-const FullRowTouchableOpacityWrapper = styled.TouchableOpacity`
+const FullRowTouchableOpacity = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
-  padding: 15px 0px;
+  padding: 15px;
   margin: 10px;
   border-radius: 8px;
   height: 10%;
-  background-color: ${props =>
-    props.background ? props.background : palette.hakgyoPurple};
-`;
-
-const ColoredText = styled.Text`
-  color: ${props => (props.color ? props.color : palette.black)};
-  font-size: 20px;
-  font-family: 'baskin-robbins-R';
+  background-color: ${({ background }) =>
+    background ? background : palette.hakgyoPurple};
 `;
 
 export default FullRowTouchableOpacity;
