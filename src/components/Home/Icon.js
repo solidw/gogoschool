d5f53a8aba6202122288aec8a0f533b0834e0dd4
solidw <solidw@naver.com>
2020-06-14 23:44:53 +0900

    refactor: Use destruct statement with es6

--- src/components/Home/Icon.js
+++ src/components/Home/Icon.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 import { Image } from 'react-native';
 
 const Icon = styled.Image`
-  width: ${props => (props.size ? props.size + 'px' : '50px')};
-  height: ${props => (props.size ? props.size + 'px' : '50px')};
+  width: ${({ size }) => (size ? size + 'px' : '50px')};
+  height: ${({ size }) => (size ? size + 'px' : '50px')};
   resize-mode: contain;
 `;
 
