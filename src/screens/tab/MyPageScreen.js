c6797ed8013e92869bbbc60d2655058352d32551
solidw <solidw@naver.com>
2020-06-16 02:53:09 +0900

    feat: Load user data and Add logout feature

--- src/screens/tab/MyPageScreen.js
+++ src/screens/tab/MyPageScreen.js
@@ -1,45 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
 import styled from 'styled-components';
 import palette from 'src/lib/palette';
 import Icon from 'src/components/Icon';
 import StyledText from 'src/components/StyledText';
-
+import FullRowTouchableOpacity from 'src/components/FullRowTouchableOpacity';
 import ProfileIcon from 'src/lib/assets/profile-icon.png';
+import { UserContext } from 'src/contexts/UserContext';
+import { AuthContext } from 'src/contexts/AuthContext';
+import { SIGN_OUT } from '../../contexts/reducers';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const MyPageScreen = () => {
+  const { user } = useContext(UserContext);
+  const { dispatch } = useContext(AuthContext);
+
   return (
     <MyPageScreenWrapper>
       <HeaderInfoView>
         <Icon source={ProfileIcon} />
-        <HeaderInfoText>고태완</HeaderInfoText>
+        <HeaderInfoText>{user.name}</HeaderInfoText>
       </HeaderInfoView>
       <DetailInfoView>
         <RowView>
           <RowText way={'left'}>학교명</RowText>
-          <RowText way={'right'}>송일초등학교</RowText>
+          <RowText way={'right'}>{user.school}</RowText>
         </RowView>
         <RowView>
           <RowText way={'left'}>학년</RowText>
-          <RowText way={'right'}>1</RowText>
+          <RowText way={'right'}>{user.grade}</RowText>
         </RowView>
         <RowView>
           <RowText way={'left'}>반</RowText>
-          <RowText way={'right'}>1</RowText>
+          <RowText way={'right'}>{user.classNo}</RowText>
         </RowView>
         <RowView>
           <RowText way={'left'}>번호</RowText>
-          <RowText way={'right'}>1</RowText>
+          <RowText way={'right'}>{user.number}</RowText>
         </RowView>
         <RowView>
           <RowText way={'left'}>이름</RowText>
-          <RowText way={'right'}>고태완</RowText>
+          <RowText way={'right'}>{user.name}</RowText>
         </RowView>
         <RowView>
           <RowText way={'left'}>인증번호</RowText>
-          <RowText way={'right'}>ABC123</RowText>
+          <RowText way={'right'}>{user.code}</RowText>
         </RowView>
       </DetailInfoView>
+      <FullRowTouchableOpacity
+        onPress={() => dispatch({ type: SIGN_OUT })}
+        background={palette.hakgyoYellow}>
+        <StyledText>로그아웃</StyledText>
+      </FullRowTouchableOpacity>
     </MyPageScreenWrapper>
   );
 };
@@ -63,9 +75,10 @@ const HeaderInfoText = styled(StyledText)`
 `;
 
 const DetailInfoView = styled.View`
+  flex: 1;
   padding-vertical: 15px;
   padding-horizontal: 20px;
-  justify-content: center;
+  justify-content: flex-start;
 `;
 
 const RowView = styled.View`
