3d3bdd553a20eb95fbd1220d42e2871a9bbc6a22
solidw <solidw@naver.com>
2020-06-16 22:27:09 +0900

    style: Remove unused value

--- src/components/StyledText.js
+++ src/components/StyledText.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { Text } from 'react-native';
 import styled from 'styled-components';
-import palette from 'src/lib/palette';
 
 const StyledText = styled.Text`
   ${({ size }) => size && `font-size: ${size}px`};
