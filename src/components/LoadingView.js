a92451276d9e363d1668742cce45ceb4c282b2f3
solidw <solidw@naver.com>
2020-06-16 22:29:30 +0900

    feat: Add gogoschool appicon and text for loadng

--- src/components/LoadingView.js
+++ src/components/LoadingView.js
@@ -3,9 +3,15 @@ import { View, Text } from 'react-native';
 import styled from 'styled-components';
 import palette from 'src/lib/palette';
 import AbsoluteIndicator from 'src/components/AbsoluteIndicator';
+import AppIcon from 'src/lib/assets/appicon.png';
+import Icon from 'src/components/Icon';
+import StyledText from 'src/components/StyledText';
+
 const LoadingView = () => {
   return (
     <LoadingViewWrapper>
+      <Icon size={300} source={AppIcon} />
+      <StyledText size={30}>학교가자 로딩중입니다.</StyledText>
       <AbsoluteIndicator size="large" />
     </LoadingViewWrapper>
   );
@@ -13,6 +19,8 @@ const LoadingView = () => {
 
 const LoadingViewWrapper = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default LoadingView;
