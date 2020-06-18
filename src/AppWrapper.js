8b838710aaa8870c387dca86df207e5d97cefb00
solidw <solidw@naver.com>
2020-06-19 02:39:27 +0900

    refactor: Move firebase message appwrapper to app

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import styled from 'styled-components';
 import AsyncStorage from '@react-native-community/async-storage';
-import messaging from '@react-native-firebase/messaging';
 
 import { AuthContext } from 'src/contexts/AuthContext';
 import { UserContext } from 'src/contexts/UserContext';
@@ -10,7 +9,6 @@ import MainTabNavigator from 'src/navigators/MainTabNavigator';
 import AuthStackNavigator from './navigators/AuthStackNavigator';
 import LoadingView from 'src/components/LoadingView';
 import { RESTORE_TOKEN } from 'src/contexts/reducers';
-import { Alert } from 'react-native';
 
 const AppWrapper = () => {
   const { authState, dispatch } = useContext(AuthContext);
@@ -31,29 +29,6 @@ const AppWrapper = () => {
     };
     asyncGetUserInfo();
   }, [dispatch, setUser]);
-
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert(
-        'Foreground: A new FCM message arrived!',
-        remoteMessage.notification.body,
-      );
-    });
-    return unsubscribe;
-  }, []);
-
-  useEffect(() => {
-    const unsubscribe = messaging().setBackgroundMessageHandler(
-      async remoteMessage => {
-        console.log(
-          'Background: Message handled in the background!',
-          remoteMessage,
-        );
-      },
-    );
-
-    return unsubscribe;
-  }, []);
 
   if (authState.isLoading) {
     return <LoadingView />;
