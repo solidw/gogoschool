5cbf4d28893fd03f091757fb06af21cbc3965e52
solidw <solidw@naver.com>
2020-06-19 02:36:22 +0900

    refactor: Move notification appwrapper to app

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
 import 'react-native-gesture-handler';
-import React, { useContext } from 'react';
+import React, { useEffect, Alert } from 'react';
+import messaging from '@react-native-firebase/messaging';
 
-import UserContextProvider, { UserContext } from 'src/contexts/UserContext';
-import AppWrapper from 'src/AppWrapper';
+import UserContextProvider from 'src/contexts/UserContext';
 import AuthContextProvider from 'src/contexts/AuthContext';
+import NoticeContextProvider from 'src/contexts/NoticeContext';
+import MissionContextProvider from 'src/contexts/MissionContext';
+import AppWrapper from 'src/AppWrapper';
 
 const App = () => {
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(
+        'Foreground: A new FCM message arrived!',
+        remoteMessage.notification.body,
+      );
+    });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().setBackgroundMessageHandler(
+      async remoteMessage => {
+        console.log(
+          'Background: Message handled in the background!',
+          remoteMessage,
+        );
+      },
+    );
+
+    return unsubscribe;
+  }, []);
+
   return (
     <AuthContextProvider>
       <UserContextProvider>
-        <AppWrapper />
+        <NoticeContextProvider>
+          <MissionContextProvider>
+            <AppWrapper />
+          </MissionContextProvider>
+        </NoticeContextProvider>
       </UserContextProvider>
     </AuthContextProvider>
   );
