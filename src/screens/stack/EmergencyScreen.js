056f7e610c0b11acd9cea894753839ec010f24c1
solidw <solidw@naver.com>
2020-06-19 13:48:42 +0900

    refactor: Remove unused value

--- src/screens/stack/EmergencyScreen.js
+++ src/screens/stack/EmergencyScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Dimensions } from 'react-native';
 import styled from 'styled-components';
 import palette from 'src/lib/palette';
