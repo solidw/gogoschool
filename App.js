bda84db81f31758c19edcc2adfc6fde5690ee6e9
solidw <solidw@naver.com>
2020-06-21 03:29:17 +0900

    refactor: Move firebase messaging app.js to appwrapper.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import 'react-native-gesture-handler';
-import React, { useEffect, Alert } from 'react';
+import React from 'react';
 import messaging from '@react-native-firebase/messaging';
-
 import UserContextProvider from 'src/contexts/UserContext';
 import AuthContextProvider from 'src/contexts/AuthContext';
 import NoticeContextProvider from 'src/contexts/NoticeContext';
@@ -9,16 +8,6 @@ import MissionContextProvider from 'src/contexts/MissionContext';
 import AppWrapper from 'src/AppWrapper';
 
 const App = () => {
-  useEffect(() => {
-    const unsubscribe = messaging().onMessage(async remoteMessage => {
-      Alert.alert(
-        'Foreground: A new FCM message arrived!',
-        remoteMessage.notification.body,
-      );
-    });
-    return unsubscribe;
-  }, []);
-
   messaging().setBackgroundMessageHandler(async remoteMessage => {
     console.log(
       'Background: Message handled in the background!',
--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,9 +1,12 @@
 import React, { useContext, useEffect } from 'react';
+import { Alert } from 'react-native';
 import styled from 'styled-components';
 import AsyncStorage from '@react-native-community/async-storage';
+import messaging from '@react-native-firebase/messaging';
 
 import { AuthContext } from 'src/contexts/AuthContext';
 import { UserContext } from 'src/contexts/UserContext';
+import { NoticeContext } from 'src/contexts/NoticeContext';
 
 import MainTabNavigator from 'src/navigators/MainTabNavigator';
 import AuthStackNavigator from './navigators/AuthStackNavigator';
@@ -12,7 +15,8 @@ import { RESTORE_TOKEN } from 'src/contexts/reducers';
 
 const AppWrapper = () => {
   const { authState, dispatch } = useContext(AuthContext);
-  const { setUser } = useContext(UserContext);
+  const { user, setUser } = useContext(UserContext);
+  const { notices, addNotice } = useContext(NoticeContext);
 
   useEffect(() => {
     const asyncGetUserInfo = () => {
@@ -29,6 +33,21 @@ const AppWrapper = () => {
     };
     asyncGetUserInfo();
   }, [dispatch, setUser]);
+
+  useEffect(() => {
+    if (user.isStudent === false) {
+      const unsubscribe = messaging().onMessage(async remoteMessage => {
+        console.log(
+          `@AppWrapper remoteMessage: ${JSON.stringify(remoteMessage)}`,
+        );
+        if (remoteMessage.notification.body && remoteMessage.data) {
+          addNotice(remoteMessage.data);
+          Alert.alert('학생 등록 감지', remoteMessage.notification.body);
+        }
+      });
+      return unsubscribe;
+    }
+  }, [addNotice, user.isStudent]);
 
   if (authState.isLoading) {
     return <LoadingView />;
