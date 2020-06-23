1bb23e15e1dc0bba15c46cecef6cbc40837c8c6c
solidw <solidw@naver.com>
2020-06-24 02:19:37 +0900

    feat: Just import CustomWebView

--- src/screens/stack/HakgyoScreen.js
+++ src/screens/stack/HakgyoScreen.js
@@ -1,48 +1,8 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { WebView } from 'react-native-webview';
-import styled from 'styled-components';
-import AbsoluteIndicator from 'src/components/AbsoluteIndicator';
-import { Platform, BackHandler } from 'react-native';
+import React from 'react';
+import CustomWebView from 'src/components/CustomWebView';
 
 const HakgyoScreen = () => {
-  const [visible, setVisible] = useState(true);
-  const [canGoBack, setCanGoBack] = useState(false);
-  const webViewRef = useRef();
-
-  const onNavigationStateChange = navState => {
-    setCanGoBack(navState.canGoBack);
-  };
-
-  const onBack = () => {
-    if (canGoBack && webViewRef) {
-      webViewRef.current.goBack();
-      return true;
-    }
-  };
-
-  useEffect(() => {
-    let backHandler;
-    if (Platform.OS === 'android') {
-      backHandler = BackHandler.addEventListener('hardwareBackPress', onBack);
-    }
-    return () => backHandler.remove();
-  });
-
-  return (
-    <HakgyoScreenWrapper>
-      <WebView
-        ref={webViewRef}
-        source={{ uri: 'https://gogo.school' }}
-        onLoadStart={() => setVisible(true)}
-        onLoad={() => setVisible(false)}
-        onNavigationStateChange={onNavigationStateChange}
-      />
-      {visible && <AbsoluteIndicator size="large" />}
-    </HakgyoScreenWrapper>
-  );
+  return <CustomWebView uri={'https://gogo.school'} />;
 };
 
-const HakgyoScreenWrapper = styled.View`
-  flex: 1;
-`;
 export default HakgyoScreen;
