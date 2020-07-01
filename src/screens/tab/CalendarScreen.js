84425156c8c1f49340f1e83d194dbac78274489e
solidw <solidw@naver.com>
2020-07-01 22:14:48 +0900

    feat: Apply api with getDailyTemperature

--- src/screens/tab/CalendarScreen.js
+++ src/screens/tab/CalendarScreen.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 import palette from 'src/lib/palette';
 import StudentCalendarScreen from 'src/screens/tab/calendar/StudentCalendarScreen';
 import TeacherCalendarScreen from 'src/screens/tab/calendar/TeacherCalendarScreen';
-const CalendarScreen = ({ route }) => {
+const CalendarScreen = ({ route, navigation }) => {
   const { user } = route.params;
-  return user.isStudent ? <StudentCalendarScreen /> : <TeacherCalendarScreen />;
+  return user.isStudent ? (
+    <StudentCalendarScreen />
+  ) : (
+    <TeacherCalendarScreen user={user} navigation={navigation} />
+  );
 };
 
 const CalendarScreenWrapper = styled.View`
