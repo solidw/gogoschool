820bdbd8469e46bf64b4fa8e9f2af50ebe62d959
solidw <solidw@naver.com>
2020-07-25 23:40:04 +0900

    feat: Enable zoom in CustomWebview

--- src/components/CustomWebView.js
+++ src/components/CustomWebView.js
@@ -4,10 +4,19 @@ import styled from 'styled-components';
 import AbsoluteIndicator from 'src/components/AbsoluteIndicator';
 import { Platform, BackHandler } from 'react-native';
 
-const CustomWebView = ({ uri, injectedJavaScript }) => {
+const CustomWebView = ({ uri, injectedJavaScript = '' }) => {
   const [visible, setVisible] = useState(true);
   const [canGoBack, setCanGoBack] = useState(false);
   const webViewRef = useRef();
+
+  const zoomJsCode = `
+    const meta = document.createElement('meta'); 
+    meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0');
+    meta.setAttribute('name', 'viewport');
+    document.getElementsByTagName('head')[0].appendChild(meta)
+  `;
+
+  const zoomedInjectedJavascript = zoomJsCode + injectedJavaScript;
 
   const onNavigationStateChange = navState => {
     setCanGoBack(navState.canGoBack);
@@ -35,7 +44,7 @@ const CustomWebView = ({ uri, injectedJavaScript }) => {
         source={{ uri }}
         onLoadStart={() => setVisible(true)}
         onLoad={() => setVisible(false)}
-        injectedJavaScript={injectedJavaScript}
+        injectedJavaScript={zoomedInjectedJavascript}
         javaScriptEnabledAndroid={true}
         onNavigationStateChange={onNavigationStateChange}
       />
